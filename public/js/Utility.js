793dcc37e8c0f910b85d39573a0676d96c822e92
René Bärnreuther <rene.baernreuther@web.de>
2018-10-30 11:35:54 +0100

    Set position to last on input field in 170 and splitit

--- public/js/Utility.js
+++ public/js/Utility.js
@@ -3,6 +3,18 @@ function msg(msg) {
 }
 
 function logout() {
-    document.cookie = "player" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = "player" + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
     location.reload();
+}
+
+function setCursorToLast(input) {
+    if (typeof input.selectionStart == "number") {
+        input.selectionStart = input.selectionEnd = input.value.length;
+    } else if (typeof input.createTextRange != "undefined") {
+        input.focus();
+        var range = input.createTextRange();
+        range.collapse(false);
+        range.select();
+    }
+
 }
